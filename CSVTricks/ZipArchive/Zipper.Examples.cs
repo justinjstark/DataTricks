86044a26793a94ecb3bb3efa5a5ebe1ca12479e3
justinjstark <fromgithub@justinjstark.com>
2016-04-29 12:35:45 -0500

    Add ZipArchive project and code

---
+++ CSVTricks/ZipArchive/Zipper.Examples.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ZipArchive
+{
+    public class Examples
+    {
+        public void ZipFiles1()
+        {
+            var file1 = new FileToZip { Name = "test1.txt", Contents = Encoding.UTF8.GetBytes("This is test1.") };
+            var file2 = new FileToZip { Name = "test2.txt", Contents = Encoding.UTF8.GetBytes("This is test2.") };
+
+            var zippedFileContents = new Zipper().ZipFiles(new List<FileToZip> { file1, file2 });
+        }
+
+        public void ZipFiles2()
+        {
+            var file1 = new FileToZip { Directory = "test", Name = "test1.txt", Contents = Encoding.UTF8.GetBytes("This is test1.") };
+            var file2 = new FileToZip { Name = "test2.txt", Contents = Encoding.UTF8.GetBytes("This is test2.") };
+
+            var zippedFileContents = new Zipper().ZipFiles(new List<FileToZip> { file1, file2 });
+        }
+    }
+}
---
+++ CSVTricks/ZipArchive/Zipper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipArchive
+{
+    public class FileToZip
+    {
+        public string Directory { get; set; }
+        public string Name { get; set; }
+        public byte[] Contents { get; set; }
+    }
+
+    public class Zipper
+    {
+        public byte[] ZipFiles(IEnumerable<FileToZip> files)
+        {
+            MemoryStream memoryStream;
+
+            using (memoryStream = new MemoryStream())
+            using (var zipArchive = new System.IO.Compression.ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+            {
+                foreach (var file in files)
+                {
+                    var newFile = zipArchive.CreateEntry(Path.Combine(file.Directory ?? "", file.Name));
+
+                    using (var newFileStream = newFile.Open())
+                    using (var binaryWriter = new BinaryWriter(newFileStream))
+                    {
+                        binaryWriter.Write(file.Contents);
+                    }
+                }
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+}
